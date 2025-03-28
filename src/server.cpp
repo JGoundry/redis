35a46a4aa3fefb880af2d7961c21c1b91f58084b
Josh Goundry <joshuagoundry@gmail.com>
2025-03-28 22:52:15 +0000

    Add .clang-format file and format existing source code.

--- src/server.cpp
+++ src/server.cpp
@@ -3,46 +3,45 @@
 #include <iostream>
 
 #include <boost/asio.hpp>
-#include <boost/asio/streambuf.hpp>
-#include <boost/asio/impl/read_until.hpp>
 #include <boost/asio/buffers_iterator.hpp>
 #include <boost/asio/connect.hpp>
+#include <boost/asio/impl/read_until.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/ip/address_v4.hpp>
+#include <boost/asio/streambuf.hpp>
 
-std::string read(boost::asio::ip::tcp::socket& socket)
+std::string read( boost::asio::ip::tcp::socket& socket )
 {
     boost::asio::streambuf buf;
-    boost::asio::read_until(socket, buf, "\n"); // async read until?
+    boost::asio::read_until( socket, buf, "\n" ); // async read until?
 
-    return std::string(boost::asio::buffers_begin(buf.data()), boost::asio::buffers_end(buf.data()));
+    return std::string( boost::asio::buffers_begin( buf.data() ), boost::asio::buffers_end( buf.data() ) );
 }
 
-void write(boost::asio::ip::tcp::socket& socket, const std::string& msg)
+void write( boost::asio::ip::tcp::socket& socket, const std::string& msg )
 {
-    boost::asio::write(socket, boost::asio::buffer(msg + "\n"));
+    boost::asio::write( socket, boost::asio::buffer( msg + "\n" ) );
 }
 
-
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
     std::cout << "Starting server...\n";
 
     boost::asio::io_context ctx;
 
-    boost::asio::ip::tcp::acceptor acceptor(ctx, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 6379));
+    boost::asio::ip::tcp::acceptor acceptor( ctx, boost::asio::ip::tcp::endpoint( boost::asio::ip::tcp::v4(), 6379 ) );
 
-    boost::asio::ip::tcp::socket socket(ctx);
+    boost::asio::ip::tcp::socket socket( ctx );
 
-    acceptor.accept(socket);
+    acceptor.accept( socket );
 
-    std::string msg = read(socket);
+    std::string msg = read( socket );
 
     std::cout << "Read message from client:\n";
     std::cout << msg << '\n';
 
-    write(socket, "Hello from server!");
+    write( socket, "Hello from server!" );
 
     return 0;
 }
